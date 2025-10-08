6a23fb8d29e285b3dc5b2ea9472733bdc4f4eddc
Manoj Kumar <iammanoj905@gmail.com>
2025-10-08 10:42:12 +0530

    commited By Ms

---
+++ src/main/java/com/example/omni/DemoProjectForGit/DemoProjectForGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.omni.DemoProjectForGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectForGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectForGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/omni/DemoProjectForGit/DemoProjectForGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.omni.DemoProjectForGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectForGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
