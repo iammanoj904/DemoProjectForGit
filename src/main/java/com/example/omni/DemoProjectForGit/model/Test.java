af226a9ddc4b3d6e0c4f24508c6b3394f6bea3fa
iammanoj904 <iammanoj904@gmail.com>
2025-10-08 10:51:44 +0530

    third time

--- src/main/java/com/example/omni/DemoProjectForGit/model/Test.java
+++ src/main/java/com/example/omni/DemoProjectForGit/model/Test.java
@@ -3,5 +3,6 @@ package com.example.omni.DemoProjectForGit.model;
 public class Test {
     void test(){
         System.out.println("Hello Everyone");
+        System.out.println("Hello All");
     }
 }
